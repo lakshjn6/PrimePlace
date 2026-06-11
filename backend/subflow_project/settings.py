c112af2191f4a57938ece3a082748dcaa9695d7f
Lakshay Mittal <lakshayjain0611@gmail.com>
2026-06-11 15:08:00 +0530

    fix setting2

--- backend/subflow_project/settings.py
+++ backend/subflow_project/settings.py
@@ -141,7 +141,7 @@ EMAIL_PORT = int(config('EMAIL_PORT', default='587'))
 EMAIL_USE_TLS = config('EMAIL_USE_TLS', default='True') == 'True'
 EMAIL_HOST_USER = config('EMAIL_HOST_USER', default='')
 EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD', default='')
-DEFAULT_FROM_EMAIL = f'PrimeMarket <{EMAIL_HOST_USER}>'
+DEFAULT_FROM_EMAIL = config('DEFAULT_FROM_EMAIL', default='noreply@primemarket.com')
 
 
 # FRONTEND URL
