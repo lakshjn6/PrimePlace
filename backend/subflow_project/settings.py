7a563d799184d54c84c0c5dbd6db220a832b86b2
Lakshay Mittal <lakshayjain0611@gmail.com>
2026-06-12 09:45:57 +0530

    ssl tls fix

--- backend/subflow_project/settings.py
+++ backend/subflow_project/settings.py
@@ -145,6 +145,7 @@ EMAIL_BACKEND = config('EMAIL_BACKEND', default='django.core.mail.backends.conso
 EMAIL_HOST = config('EMAIL_HOST', default='smtp.gmail.com')
 EMAIL_PORT = int(config('EMAIL_PORT', default='587'))
 EMAIL_USE_TLS = config('EMAIL_USE_TLS', default='True') == 'True'
+EMAIL_USE_SSL = config('EMAIL_USE_SSL', default='False') == 'True' 
 EMAIL_HOST_USER = config('EMAIL_HOST_USER', default='')
 EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD', default='')
 DEFAULT_FROM_EMAIL = config('DEFAULT_FROM_EMAIL', default='noreply@primemarket.com')
