55971b85962acbf31662aabe4eb1ce3f07a429a9
Lakshay Mittal <lakshayjain0611@gmail.com>
2026-06-12 21:48:11 +0530

    billing update

--- backend/store/models.py
+++ backend/store/models.py
@@ -25,10 +25,20 @@ class Category(models.Model):
 
 class Product(models.Model):
     BILLING_CHOICES = [
-        ('monthly', 'Monthly'),
-        ('yearly',  'Yearly'),
-        ('lifetime','Lifetime'),
-    ]
+    ('1_month', '1 Month'),
+    ('2_months', '2 Months'),
+    ('3_months', '3 Months'),
+    ('4_months', '4 Months'),
+    ('5_months', '5 Months'),
+    ('6_months', '6 Months'),
+    ('7_months', '7 Months'),
+    ('8_months', '8 Months'),
+    ('9_months', '9 Months'),
+    ('10_months', '10 Months'),
+    ('11_months', '11 Months'),
+    ('yearly', 'Yearly'),
+    ('24_months', '24 Months'),
+    ('lifetime', 'Lifetime'),]
 
     category       = models.ForeignKey(Category, on_delete=models.CASCADE,
                                        related_name='products')
